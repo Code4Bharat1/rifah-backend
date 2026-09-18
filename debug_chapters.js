719010511113e2823546ec1398b19a2094cf80fb
Azeem <azeemnoor5226@gmail.com>
2026-09-18 10:39:15 +0530

    chore: add debug script to test chapter details retrieval

---
+++ debug_chapters.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { chapterService } from './src/modules/chapters/chapter.service.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(async () => {
+    try {
+      const details = await chapterService.getChapterDetails("6aab6d61504e19d5003c4a02", { role: 'super_admin' });
+      console.log("Details found:", Object.keys(details));
+    } catch (err) {
+      console.error("Error fetching details:", err);
+    }
+    
+    process.exit(0);
+  });
